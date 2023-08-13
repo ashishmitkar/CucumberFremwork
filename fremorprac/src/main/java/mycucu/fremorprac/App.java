9df137687608fe46fc85b28988686c8837fadb5b
Hp <Hp@DESKTOP-O9N0DQK>
2023-08-13 19:12:52 +0530

    initial Commit.

---
+++ fremorprac/src/main/java/mycucu/fremorprac/App.java
@@ -0,0 +1,13 @@
+package mycucu.fremorprac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
